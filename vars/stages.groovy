7e4301a2caf99ce443c92c87ec8f4e93539d6c1b
RitikaM09 <90257344+RitikaM09@users.noreply.github.com>
2021-10-04 17:30:17 +0530

    Update stages.groovy

--- vars/stages.groovy
+++ vars/stages.groovy
@@ -1,1 +1,10 @@
-
+def initStage(def caller)
+{
+  def inputParameters="component_name:wallets;environment:d1-pmgt;git_repo:git@github.sie.sony.com:SIE/ca-hyperloop.git;project_dir:property/;testscope:Promote;git__branch:gisc-kt;"
+  
+  String [] params= inputParameters.split(";")
+  
+  println "Params : ${params}"
+  
+  util.parseInputParams(caller,params)
+}
